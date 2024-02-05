7556d78dc324046a164136bb374052f97385c289
Omer <omerbirol11@gmail.com>
2024-02-05 16:18:42 +0300

    aveshatasızgibi

---
+++ exportaves/app/src/main/java/com/example/exportaves/button_main_activity.java
@@ -0,0 +1,58 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		button_main
+	 *	@date 		Tuesday 19th of December 2023 05:26:08 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.exportaves;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class button_main_activity extends Activity {
+
+	
+	private View _bg__button_main;
+	private View _bg__circle_ek1;
+	private ImageView clip_path_group;
+	private ImageView icon_08;
+	private TextView yardim__ste;
+	private TextView adres_bilgilerimi_g_ncelle;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.button_main);
+
+		
+		_bg__button_main = (View) findViewById(R.id._bg__button_main);
+		_bg__circle_ek1 = (View) findViewById(R.id._bg__circle_ek1);
+		clip_path_group = (ImageView) findViewById(R.id.clip_path_group);
+		icon_08 = (ImageView) findViewById(R.id.icon_08);
+		yardim__ste = (TextView) findViewById(R.id.yardim__ste);
+		adres_bilgilerimi_g_ncelle = (TextView) findViewById(R.id.adres_bilgilerimi_g_ncelle);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ exportaves/app/src/main/java/com/example/exportaves/start_menu_activity.java
@@ -0,0 +1,68 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		start_menu
+	 *	@date 		Tuesday 19th of December 2023 05:21:35 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.exportaves;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class start_menu_activity extends Activity {
+
+	
+	private View _bg__start_menu;
+	private TextView aves_iha_yard_m;
+	private TextView acil_durumlarda_ihtiyac_n_z_olanlar__size_ula_t_r_yoruz_;
+	private ImageView istockphoto_1009279168_170667a_1;
+	private View _bg__loginmainbutton_ek1;
+	private ImageView rectangle_3;
+	private TextView giri__yap;
+	private View _bg__signupmainbutton_ek1;
+	private View rectangle_4;
+	private TextView kay_t_ol;
+	private TextView uygulama_nas_l_kullan_l_r;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start_menu);
+
+		
+		_bg__start_menu = (View) findViewById(R.id._bg__start_menu);
+		aves_iha_yard_m = (TextView) findViewById(R.id.aves_iha_yard_m);
+		acil_durumlarda_ihtiyac_n_z_olanlar__size_ula_t_r_yoruz_ = (TextView) findViewById(R.id.acil_durumlarda_ihtiyac_n_z_olanlar__size_ula_t_r_yoruz_);
+		istockphoto_1009279168_170667a_1 = (ImageView) findViewById(R.id.istockphoto_1009279168_170667a_1);
+		_bg__loginmainbutton_ek1 = (View) findViewById(R.id._bg__loginmainbutton_ek1);
+		rectangle_3 = (ImageView) findViewById(R.id.rectangle_3);
+		giri__yap = (TextView) findViewById(R.id.giri__yap);
+		_bg__signupmainbutton_ek1 = (View) findViewById(R.id._bg__signupmainbutton_ek1);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		kay_t_ol = (TextView) findViewById(R.id.kay_t_ol);
+		uygulama_nas_l_kullan_l_r = (TextView) findViewById(R.id.uygulama_nas_l_kullan_l_r);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
